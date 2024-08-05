e953acd9976491c374a95085c1ce85f1ab96df67
franconubi <franco.galoppo@nubiral.com>
2024-08-05 16:31:59 -0300

    update cambios deploy storage

--- infra/main.tf
+++ infra/main.tf
@@ -3,28 +3,42 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
-  location = "East US"
+  name     = "myResourceGroup"  # Reemplaza "myResourceGroup" con el nombre deseado para tu grupo de recursos
+  location = "East US"  # Reemplaza "East US" con la región deseada
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "mystorageacct"  # Reemplaza "mystorageacct" con un nombre único para la cuenta de almacenamiento
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "terraform-state"  # Reemplaza "terraform-state" si deseas un nombre diferente para el contenedor
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
 }
 
 resource "azurerm_app_service_plan" "asp" {
-  name                = "myAppServicePlan"
+  name                = "myAppServicePlan"  # Reemplaza "myAppServicePlan" con el nombre deseado para el plan de servicio
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
-    tier     = "Free"
-    size     = "F1"
+    tier = "Free"  # Mantén "Free" para evitar costos, ya que es un ejercicio de prueba
+    size = "F1"
   }
 }
 
 resource "azurerm_app_service" "app" {
-  name                = "myAppService"
+  name                = "myAppService"  # Reemplaza "myAppService" con el nombre deseado para la aplicación web
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
 
   site_config {
-    app_command_line = "dotnet ./myapp.dll"
+    app_command_line = "npm start"  # Reemplaza "npm start" con el comando que tu aplicación necesita para iniciar
   }
 
   app_settings = {
@@ -36,4 +50,14 @@ resource "azurerm_app_service" "app" {
   ]
 }
 
-test
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_primary_access_key" {
+  value = azurerm_storage_account.sa.primary_access_key
+}
