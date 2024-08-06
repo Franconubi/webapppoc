1d0cceaa97e5e73ff7ede1372c608339bd23b56b
franconubi <franco.galoppo@nubiral.com>
2024-08-06 16:51:19 -0300

    prueba de terraform

--- infra/main.tf
+++ infra/main.tf
@@ -3,14 +3,16 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = "webapppocfranc-resources"
+  location = "westeurope"
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
+  name                = "webapppocfranc-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  kind                = "Windows"
+
   sku {
     tier = "Basic"
     size = "B1"
@@ -18,17 +20,29 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = "example-appservice"
+  name                = "webapppocfranc-appservice"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
-  site_config {
-    always_on = true
-  }
-
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
+
+  site_config {
+    always_on = true
+  }
 }
 
+resource "azurerm_app_service_slot" "example" {
+  name                = "poc"
+  location            = azurerm_app_service.example.location
+  resource_group_name = azurerm_app_service.example.resource_group_name
+  app_service_name    = azurerm_app_service.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  app_settings        = azurerm_app_service.example.app_settings
+
+  site_config {
+    always_on = true
+  }
+}
