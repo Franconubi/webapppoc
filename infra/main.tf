fc0442c34643c0154d372fd7e500e01a051c62c3
franconubi <franco.galoppo@nubiral.com>
2024-08-06 17:05:11 -0300

    se agregan los tags

--- infra/main.tf
+++ infra/main.tf
@@ -5,23 +5,38 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "webapppocfranc-resources"
   location = "westeurope"
+
+  tags = {
+    Owner    = "Pod2"
+    CreatedBy = "FrancoGaloppo"
+    Project  = "labs"
+    Pod      = "2"
+    Deadline = "SERVICIOS"
+  }
 }
 
 resource "azurerm_app_service_plan" "example" {
   name                = "webapppocfranc-appserviceplan"
-  location            = azurerm_resource_group.example.location
+  location            = "westeurope"
   resource_group_name = azurerm_resource_group.example.name
   kind                = "Windows"
-
   sku {
     tier = "Basic"
     size = "B1"
+  }
+
+  tags = {
+    Owner    = "Pod2"
+    CreatedBy = "FrancoGaloppo"
+    Project  = "labs"
+    Pod      = "2"
+    Deadline = "SERVICIOS"
   }
 }
 
 resource "azurerm_app_service" "example" {
   name                = "webapppocfranc-appservice"
-  location            = azurerm_resource_group.example.location
+  location            = "westeurope"
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
@@ -29,20 +44,11 @@ resource "azurerm_app_service" "example" {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
 
-  site_config {
-    always_on = true
-  }
-}
-
-resource "azurerm_app_service_slot" "example" {
-  name                = "poc"
-  location            = azurerm_app_service.example.location
-  resource_group_name = azurerm_app_service.example.resource_group_name
-  app_service_name    = azurerm_app_service.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-  app_settings        = azurerm_app_service.example.app_settings
-
-  site_config {
-    always_on = true
+  tags = {
+    Owner    = "Pod2"
+    CreatedBy = "FrancoGaloppo"
+    Project  = "labs"
+    Pod      = "2"
+    Deadline = "SERVICIOS"
   }
 }
